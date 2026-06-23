eea61dbb1c65b583d82bbbbd955b14a6ca8d73b4
srinivasreddyt <srinivasreddytalla333@gmail.com>
2026-06-24 00:01:58 +0530

    Fix CORS to allow all origins

--- api/create-order.js
+++ api/create-order.js
@@ -1,7 +1,7 @@
 const Razorpay = require('razorpay');
 
 module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://maathruthva.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
--- api/verify-payment.js
+++ api/verify-payment.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 
 module.exports = (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://maathruthva.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
